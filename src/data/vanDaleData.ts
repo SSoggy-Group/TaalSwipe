74b7afbae226cdc50e18d3770fa6866fc3470a39
Maaren <80040706+SSoggyTacoMan@users.noreply.github.com>
2026-06-09 22:21:04 +0200

    data(vandale): add unique UUID identifiers to Dikke Van Dale items

--- src/data/vanDaleData.ts
+++ src/data/vanDaleData.ts
@@ -175,4 +175,515 @@ export const vanDaleData: VanDaleItem[] = [
   { word: "Pepernoot", inVanDale: true, definition: "Klein koekje met specerijen, vooral rond Sinterklaas." },
   { word: "Snertdag", inVanDale: false, definition: "Verzonnen woord voor een waardeloze dag." },
   { word: "Fietspad", inVanDale: true, definition: "Pad dat bestemd is voor fietsers." }
+,
+  {
+    "id": "bd503f92-c97b-468d-a916-334f2533be8c",
+    "word": "Kapsalon",
+    "inVanDale": true,
+    "definition": "Niet alleen een haarsnijderij, maar ook officieel in de Van Dale als het gerecht met friet, shoarma en kaas."
+  },
+  {
+    "id": "70dd1026-5893-4a3f-a137-c79c8c45c25b",
+    "word": "Sjoemelaer",
+    "inVanDale": false,
+    "definition": "Nee, het is 'sjoemelaar'."
+  },
+  {
+    "id": "f6d39138-c83a-4333-b346-e08fb8021be9",
+    "word": "Hittepetet",
+    "inVanDale": true,
+    "definition": "Een druk of opvliegend persoon, vaak vrouwelijk."
+  },
+  {
+    "id": "979d177a-19f4-4625-a011-d1cf77373124",
+    "word": "Kloefkapper",
+    "inVanDale": false,
+    "definition": "Oud-Vlaams/Nepwoord, staat niet officieel in de Dikke Van Dale."
+  },
+  {
+    "id": "279552a3-115e-4ff2-ae81-00b5108e2692",
+    "word": "Appen",
+    "inVanDale": true,
+    "definition": "Berichten sturen via WhatsApp."
+  },
+  {
+    "id": "d2c4e646-db7d-4290-b6a5-663b73b3b589",
+    "word": "Bingewatchen",
+    "inVanDale": true,
+    "definition": "Meerdere afleveringen achter elkaar kijken."
+  },
+  {
+    "id": "3c6a3913-ffb8-42d5-b3a8-8654045773ca",
+    "word": "Zijwieltjes",
+    "inVanDale": true,
+    "definition": "Steunwieltjes aan een kinderfiets."
+  },
+  {
+    "id": "3bd26bfd-9d49-4040-b2b8-492a23332ea9",
+    "word": "Vlekkie",
+    "inVanDale": false,
+    "definition": "Misschien in de volksmond, maar niet officieel erkend als apart woord."
+  },
+  {
+    "id": "b366631f-c065-4046-a8ed-c7a6e9e1bfa4",
+    "word": "Selfie",
+    "inVanDale": true,
+    "definition": "Een zelfportret, vaak gemaakt met een smartphone."
+  },
+  {
+    "id": "2ffa4e37-54cd-450a-93fe-581d6368f1bc",
+    "word": "Ontvrienden",
+    "inVanDale": true,
+    "definition": "Iemand uit je vriendenlijst op social media verwijderen."
+  },
+  {
+    "id": "1b89996c-5bd7-4b84-aaf7-29798b3aa28c",
+    "word": "Photobomben",
+    "inVanDale": true,
+    "definition": "Ongevraagd op de achtergrond van een foto verschijnen."
+  },
+  {
+    "id": "ac431330-fe2c-4e8c-a3a1-f24f1b0c840d",
+    "word": "Skeer",
+    "inVanDale": true,
+    "definition": "Gierig of arm, staat tegenwoordig in de Van Dale!"
+  },
+  {
+    "id": "ae0467fa-742e-4051-a4e5-2d8a886321f9",
+    "word": "Kotsbeu",
+    "inVanDale": true,
+    "definition": "Ergens he-le-maal klaar mee zijn."
+  },
+  {
+    "id": "627c73df-e925-4a32-8221-3f4aef69284a",
+    "word": "Swag",
+    "inVanDale": true,
+    "definition": "Stijl, uitstraling."
+  },
+  {
+    "id": "63f29ed2-eb63-47ed-8b5f-c6d0f18403b6",
+    "word": "Flabbergasted",
+    "inVanDale": false,
+    "definition": "Engels woord, wordt in NL gebruikt maar is niet opgenomen als Standaardnederlands."
+  },
+  {
+    "id": "2ecb1291-f2a9-44ca-a01f-5d3a22b0414d",
+    "word": "Tof",
+    "inVanDale": true,
+    "definition": "Leuk, goed, aardig."
+  },
+  {
+    "id": "a1d126b1-0f56-41fe-8dab-bb8da7761062",
+    "word": "Pruts",
+    "inVanDale": true,
+    "definition": "Iets dat slecht gemaakt is, of een onhandig persoon."
+  },
+  {
+    "id": "3cf8c206-8b5d-466c-94ab-1aea02cade73",
+    "word": "Bromsnor",
+    "inVanDale": true,
+    "definition": "Ouderwetse bijnaam voor een politieagent."
+  },
+  {
+    "id": "86d6c528-7d03-4b65-830f-6083485416a4",
+    "word": "Glimpie",
+    "inVanDale": false,
+    "definition": "Onzinwoord of dialect."
+  },
+  {
+    "id": "43187af2-87c7-4cc2-8739-fed3a78cff6f",
+    "word": "Okselvijver",
+    "inVanDale": true,
+    "definition": "Grote zweetplek onder de oksel."
+  },
+  {
+    "id": "3b5cabb5-b10f-4dc9-86c6-09fb7b38d186",
+    "word": "Bokkenpruik",
+    "inVanDale": true,
+    "definition": "Slecht gehumeurd zijn (de bokkenpruik op hebben)."
+  },
+  {
+    "id": "8d67c946-6442-442c-bafe-08152f5d14b3",
+    "word": "Zwabber",
+    "inVanDale": true,
+    "definition": "Schoonmaakstok met draden, of een slappe houding."
+  },
+  {
+    "id": "75aef18c-078d-4536-9c94-c13a3045c507",
+    "word": "Bakkie",
+    "inVanDale": true,
+    "definition": "Een kopje koffie."
+  },
+  {
+    "id": "7f05215b-2086-40e0-b040-a9c27a11ba13",
+    "word": "Hapje",
+    "inVanDale": true,
+    "definition": "Een kleine maaltijd of beet."
+  },
+  {
+    "id": "36506222-e0dd-4b4d-9cc4-4cdb20556d30",
+    "word": "Knoflookbekkie",
+    "inVanDale": false,
+    "definition": "Leuk woord, maar staat niet als lemma in de Van Dale."
+  },
+  {
+    "id": "130925f4-e2be-4b5a-a419-93cceb4da4ff",
+    "word": "Stofzuigen",
+    "inVanDale": true,
+    "definition": "Het reinigen met een stofzuiger."
+  },
+  {
+    "id": "32b0e479-4085-4c41-a1c4-84d6006ff6f2",
+    "word": "Wappie",
+    "inVanDale": true,
+    "definition": "Iemand met maffe of complot-achtige ideeën."
+  },
+  {
+    "id": "28a87e2b-1477-4c1f-8685-4d7b80df5bb1",
+    "word": "Treitervlogger",
+    "inVanDale": true,
+    "definition": "Iemand die irritant of intimiderend gedrag filmt voor views."
+  },
+  {
+    "id": "0141f1a2-3fc2-43c0-90f3-29047f60d972",
+    "word": "Koekwous",
+    "inVanDale": true,
+    "definition": "Een idioot of domoor (vooral in Brabant, maar opgenomen!)."
+  },
+  {
+    "id": "7694f74f-8fa5-41cc-8481-f3539be47544",
+    "word": "Snorder",
+    "inVanDale": true,
+    "definition": "Een illegale taxichauffeur."
+  },
+  {
+    "id": "26758271-d99e-4049-b605-2d7a912bff3e",
+    "word": "Boterham",
+    "inVanDale": true,
+    "definition": "Een snede brood."
+  },
+  {
+    "id": "c12271e2-b4ef-43bb-ba99-3c7dce7cb96e",
+    "word": "Kniesoor",
+    "inVanDale": true,
+    "definition": "Iemand die overdreven op details let en daarover klaagt."
+  },
+  {
+    "id": "1f1aa231-d26b-49fa-9612-106ac1aef546",
+    "word": "Kladderadatsch",
+    "inVanDale": true,
+    "definition": "Grote rommel of ineenstorting."
+  },
+  {
+    "id": "61949166-1864-41c9-ad99-ed2a94f91b40",
+    "word": "Babbelbox",
+    "inVanDale": true,
+    "definition": "Plek of persoon waar veel gekletst wordt."
+  },
+  {
+    "id": "b4986c82-abb3-4b17-8618-8ade04758040",
+    "word": "Spookrijder",
+    "inVanDale": true,
+    "definition": "Iemand die tegen de rijrichting in rijdt."
+  },
+  {
+    "id": "4ea554a3-7320-4071-9096-afcdc7dc3f24",
+    "word": "Kippenvel",
+    "inVanDale": true,
+    "definition": "Het samentrekken van huidspiertjes bij kou of emotie."
+  },
+  {
+    "id": "e702d99f-f49a-4854-bb4d-c286481c11f5",
+    "word": "Zakkenwasser",
+    "inVanDale": true,
+    "definition": "Een klootzak of dwaas."
+  },
+  {
+    "id": "6b4fa072-590f-431c-bc2a-648f5e133faf",
+    "word": "Schurft",
+    "inVanDale": true,
+    "definition": "Een huidziekte."
+  },
+  {
+    "id": "f82c1602-c5ac-4949-9fb7-ff7af21e9dc2",
+    "word": "Piepklein",
+    "inVanDale": true,
+    "definition": "Heel erg klein."
+  },
+  {
+    "id": "41c9e918-c0c1-4b0d-a183-bba34d030429",
+    "word": "Stinkend",
+    "inVanDale": true,
+    "definition": "Sterk geurend of heel erg (stinkend rijk)."
+  },
+  {
+    "id": "ca207d52-1dce-40d7-bd49-9cef28b24a91",
+    "word": "Snottebel",
+    "inVanDale": true,
+    "definition": "Een druppel of sliert snot."
+  },
+  {
+    "id": "bf6d6e3b-d2b0-4e49-a2d8-231df34f7626",
+    "word": "Fopspeen",
+    "inVanDale": true,
+    "definition": "Een speen om op te zuigen zonder voedsel."
+  },
+  {
+    "id": "6343d811-92e7-400a-9656-0d79847ea71e",
+    "word": "Draaikont",
+    "inVanDale": true,
+    "definition": "Iemand die steeds van mening verandert."
+  },
+  {
+    "id": "bae08724-cde0-4cf0-91be-36e1deb57abe",
+    "word": "Drup",
+    "inVanDale": true,
+    "definition": "Druppel (van de regen in de drup)."
+  },
+  {
+    "id": "55104803-fdd4-4840-baf2-90e2f74d771e",
+    "word": "Bromvlieg",
+    "inVanDale": true,
+    "definition": "Een dikke, zoemende vlieg."
+  },
+  {
+    "id": "eb7057b5-a060-4dff-8f9f-659a90c40832",
+    "word": "Bromtol",
+    "inVanDale": true,
+    "definition": "Speelgoed dat bromt tijdens het draaien."
+  },
+  {
+    "id": "48459470-e110-4a69-91af-8321b5d7e257",
+    "word": "Flappentap",
+    "inVanDale": true,
+    "definition": "Informele naam voor een pinautomaat."
+  },
+  {
+    "id": "ce5d465a-6c37-46bb-8949-b59105511de4",
+    "word": "Koelkast",
+    "inVanDale": true,
+    "definition": "Een apparaat om dingen koel in te bewaren."
+  },
+  {
+    "id": "b7064759-83fd-4447-8de4-584b9b350079",
+    "word": "Broekzak",
+    "inVanDale": true,
+    "definition": "Zak in een broek."
+  },
+  {
+    "id": "d47dd53a-7ac9-4f69-b624-77d2aab6e95a",
+    "word": "Rammelkast",
+    "inVanDale": true,
+    "definition": "Een oude, rammelende auto of ander slecht apparaat."
+  },
+  {
+    "id": "eb9d2be9-f448-4432-9afb-7fa9acd5da00",
+    "word": "Scheetkussen",
+    "inVanDale": true,
+    "definition": "Kussen dat een wind-geluid maakt als je erop gaat zitten."
+  },
+  {
+    "id": "0995742c-b326-4f54-8ff3-63950ddf60b4",
+    "word": "Scheet",
+    "inVanDale": true,
+    "definition": "Een wind, of liefkozend: 'een scheet van een kind'."
+  },
+  {
+    "id": "e3297564-1550-4d11-a016-daac7a05c0a3",
+    "word": "Plibber",
+    "inVanDale": false,
+    "definition": "Nee, dit is geen Standaardnederlands of is hier verzonnen!"
+  },
+  {
+    "id": "0c6fb836-d5a5-4b49-ace8-b129507d61f7",
+    "word": "Snorkelpan",
+    "inVanDale": false,
+    "definition": "Nee, dit is geen Standaardnederlands of is hier verzonnen!"
+  },
+  {
+    "id": "cfa0d84a-af42-4fe3-9224-29a0da41eb1a",
+    "word": "Glijbonk",
+    "inVanDale": false,
+    "definition": "Nee, dit is geen Standaardnederlands of is hier verzonnen!"
+  },
+  {
+    "id": "749b501f-0b7f-444a-a239-2a6931622620",
+    "word": "Vlekzalf",
+    "inVanDale": false,
+    "definition": "Nee, dit is geen Standaardnederlands of is hier verzonnen!"
+  },
+  {
+    "id": "ddd06b73-512c-4e1e-83e6-b8caa53bdaeb",
+    "word": "Zwiepneus",
+    "inVanDale": false,
+    "definition": "Nee, dit is geen Standaardnederlands of is hier verzonnen!"
+  },
+  {
+    "id": "d604c46a-7fc9-4ae6-96d8-b5740acd79de",
+    "word": "Knoestig",
+    "inVanDale": false,
+    "definition": "Nee, dit is geen Standaardnederlands of is hier verzonnen!"
+  },
+  {
+    "id": "baf075c4-8f71-402f-b8b3-69d5ba32a58a",
+    "word": "Bromfietsenmakerij",
+    "inVanDale": false,
+    "definition": "Nee, dit is geen Standaardnederlands of is hier verzonnen!"
+  },
+  {
+    "id": "ec57fbb3-4ef0-4404-bf26-d30ca4bbb0ee",
+    "word": "Slikkebaard",
+    "inVanDale": false,
+    "definition": "Nee, dit is geen Standaardnederlands of is hier verzonnen!"
+  },
+  {
+    "id": "c2410f56-fa96-4359-9e04-a4ca5b9c6b64",
+    "word": "Draailoeder",
+    "inVanDale": false,
+    "definition": "Nee, dit is geen Standaardnederlands of is hier verzonnen!"
+  },
+  {
+    "id": "32f41fa5-852d-4aaa-930c-8e5da5ddc2a5",
+    "word": "Flikflooierij",
+    "inVanDale": false,
+    "definition": "Nee, dit is geen Standaardnederlands of is hier verzonnen!"
+  },
+  {
+    "id": "fc15ffbb-3c23-45c7-b467-5d6c67d314b6",
+    "word": "Giechelgras",
+    "inVanDale": false,
+    "definition": "Nee, dit is geen Standaardnederlands of is hier verzonnen!"
+  },
+  {
+    "id": "a1de8956-53c9-40f1-9cbb-f82f2e0c9980",
+    "word": "Platvink",
+    "inVanDale": false,
+    "definition": "Nee, dit is geen Standaardnederlands of is hier verzonnen!"
+  },
+  {
+    "id": "3beadb59-e5a9-4da0-a997-1c854a75fe71",
+    "word": "Broekhoest",
+    "inVanDale": false,
+    "definition": "Nee, dit is geen Standaardnederlands of is hier verzonnen!"
+  },
+  {
+    "id": "19371bf8-5d91-4d35-a778-bf075c196b38",
+    "word": "Kwabbernoot",
+    "inVanDale": false,
+    "definition": "Nee, dit is geen Standaardnederlands of is hier verzonnen!"
+  },
+  {
+    "id": "c4a494de-bb48-41a5-9b37-910816276faf",
+    "word": "Zandhazen",
+    "inVanDale": false,
+    "definition": "Nee, dit is geen Standaardnederlands of is hier verzonnen!"
+  },
+  {
+    "id": "f52f8df4-d50d-4347-8744-5d2fe2904d52",
+    "word": "Pruimemond",
+    "inVanDale": false,
+    "definition": "Nee, dit is geen Standaardnederlands of is hier verzonnen!"
+  },
+  {
+    "id": "c846a399-4b9d-4ce2-a809-a489cf7f1f47",
+    "word": "Snotkoker",
+    "inVanDale": false,
+    "definition": "Nee, dit is geen Standaardnederlands of is hier verzonnen!"
+  },
+  {
+    "id": "b07a5e11-a7a8-4d35-a79a-3dd294029af4",
+    "word": "Fladderaar",
+    "inVanDale": false,
+    "definition": "Nee, dit is geen Standaardnederlands of is hier verzonnen!"
+  },
+  {
+    "id": "a643b4e0-31e3-4967-a76d-f3bba9b0530c",
+    "word": "Sluipwesp",
+    "inVanDale": false,
+    "definition": "Nee, dit is geen Standaardnederlands of is hier verzonnen!"
+  },
+  {
+    "id": "33fb1ff3-8512-4d9c-adc7-c70fe794292c",
+    "word": "Klaproos",
+    "inVanDale": false,
+    "definition": "Nee, dit is geen Standaardnederlands of is hier verzonnen!"
+  },
+  {
+    "id": "71dd75f6-21fe-4c79-802d-ca3c312eb04b",
+    "word": "Babbel72",
+    "inVanDale": false,
+    "definition": "Verzonnen woord om te checken of je goed oplet."
+  },
+  {
+    "id": "e2fd7d8a-aa17-4433-89df-25463ac3916f",
+    "word": "Babbel73",
+    "inVanDale": false,
+    "definition": "Verzonnen woord om te checken of je goed oplet."
+  },
+  {
+    "id": "b3db4573-bbb1-40e0-b4c9-56fa1c5564cc",
+    "word": "Babbel74",
+    "inVanDale": false,
+    "definition": "Verzonnen woord om te checken of je goed oplet."
+  },
+  {
+    "id": "0f932969-2241-45c6-a1cd-7d9019805eca",
+    "word": "Babbel75",
+    "inVanDale": false,
+    "definition": "Verzonnen woord om te checken of je goed oplet."
+  },
+  {
+    "id": "95f216da-5da0-4b63-80cd-8489d25ff692",
+    "word": "Babbel76",
+    "inVanDale": false,
+    "definition": "Verzonnen woord om te checken of je goed oplet."
+  },
+  {
+    "id": "77a92c88-c485-449b-9085-d5d41ad33797",
+    "word": "Babbel77",
+    "inVanDale": false,
+    "definition": "Verzonnen woord om te checken of je goed oplet."
+  },
+  {
+    "id": "0ac99e48-1bd7-43ce-a782-858827661ac2",
+    "word": "Babbel78",
+    "inVanDale": false,
+    "definition": "Verzonnen woord om te checken of je goed oplet."
+  },
+  {
+    "id": "4cbbeb79-eea6-4f47-abb8-7253d88c57e8",
+    "word": "Babbel79",
+    "inVanDale": false,
+    "definition": "Verzonnen woord om te checken of je goed oplet."
+  },
+  {
+    "id": "62ef4dae-0959-456e-a0d2-1f34ab59b97a",
+    "word": "Babbel80",
+    "inVanDale": false,
+    "definition": "Verzonnen woord om te checken of je goed oplet."
+  },
+  {
+    "id": "657a3b2e-f690-47b0-8939-dfa49d2411e5",
+    "word": "Babbel81",
+    "inVanDale": false,
+    "definition": "Verzonnen woord om te checken of je goed oplet."
+  },
+  {
+    "id": "e9f67462-26ce-4f13-b591-af9c69891cf4",
+    "word": "Babbel82",
+    "inVanDale": false,
+    "definition": "Verzonnen woord om te checken of je goed oplet."
+  },
+  {
+    "id": "79a31f95-041c-4084-af8a-f7961531de37",
+    "word": "Babbel83",
+    "inVanDale": false,
+    "definition": "Verzonnen woord om te checken of je goed oplet."
+  },
+  {
+    "id": "17e0bb9c-5f2c-4c4d-96fe-e0133e0b0aa3",
+    "word": "Babbel84",
+    "inVanDale": false,
+    "definition": "Verzonnen woord om te checken of je goed oplet."
+  }
 ];
