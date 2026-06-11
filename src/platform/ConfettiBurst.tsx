8e8e3f890f310fbcbd19e8878299163f065b9c3e
Maaren <80040706+SSoggyTacoMan@users.noreply.github.com>
2026-06-11 12:08:50 +0200

    perf(ConfettiBurst): disable confetti cannon on web to resolve 20fps glitching

--- src/platform/ConfettiBurst.tsx
+++ src/platform/ConfettiBurst.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Platform } from 'react-native';
 import ConfettiCannon, { ExplosionProps } from 'react-native-confetti-cannon';
 
 export function ConfettiBurst(props: Readonly<ExplosionProps>) {
+  if (Platform.OS === 'web') return null;
   return <ConfettiCannon {...props} />;
 }
